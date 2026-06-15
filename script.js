03dd9ca8bf9f35686627852f4500414c6768bfa1
vennilaa <vennilaachandran27@gmail.com>
2026-06-15 15:18:07 +0800

    Fix Netlify

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
 function paparMaklumat() {
+
     let nama = document.getElementById("nama").value;
     let program = document.getElementById("program").value;
     let email = document.getElementById("email").value;
 
-    if (nama === "" || email === "") {
-        alert("Sila isi semua maklumat!");
-        return;
-    }
-
     document.getElementById("output").innerHTML = `
         <h3>Maklumat Pelajar</h3>
-        <p><b>Nama:</b> ${nama}</p>
-        <p><b>Program:</b> ${program}</p>
-        <p><b>Email:</b> ${email}</p>
+        <p>Nama: ${nama}</p>
+        <p>Program: ${program}</p>
+        <p>Email: ${email}</p>
     `;
 }
